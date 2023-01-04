1ff50fede6c738400a03e838d4d2d1ea21429e3e
edu.maor <edmaor128@gmail.com>
2023-01-04 16:15:45 +0100

    Minor changes

--- src/main/java/edu/maor/sorting/HeapSort.java
+++ src/main/java/edu/maor/sorting/HeapSort.java
@@ -1,40 +1,34 @@
-package edu.maor;
+package edu.maor.sorting;
 
 public class HeapSort {
-    void heapifyMax(int[] arr, int len, int i) {
+    void heapifyMax(int[] nums, int len, int i) {
         int largest = i;
         int lc = 2 * i + 1; // Get left child
         int rc = 2 * i + 2; // Get right child
-
-        if (lc < len && arr[lc] > arr[largest]) largest = lc;
-        if (rc < len && arr[rc] > arr[largest]) largest = rc;
+        if (lc < len && nums[lc] > nums[largest]) largest = lc;
+        if (rc < len && nums[rc] > nums[largest]) largest = rc;
         // If largest is not root
         if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-            // Recursively heapify the affected subtree
-            heapifyMax(arr, len, largest);
+            int swap = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = swap;
+            heapifyMax(nums, len, largest);
         }
     }
-
     public void sortMinMax(int[] nums){
         int len = nums.length;
         for (int i = len / 2 - 1; i >= 0; i--) heapifyMax(nums, len, i);
-
         for (int i = len - 1; i > 0; i--) {
             int temp = nums[0];
             nums[0] = nums[i];
             nums[i] = temp;
-            // call heapifyMax on the reduced heap
             heapifyMax(nums, i, 0);
         }
     }
-
     public static void main(String[] args) {
         int[] nums = { 12, 11, 13, 5, 6, 7, 20, 12, 10, 16, 8 };
-        HeapSort ob = new HeapSort();
-        ob.sortMinMax(nums);
+        HeapSort heapSort = new HeapSort();
+        heapSort.sortMinMax(nums);
 
         System.out.println("Sorted array is");
         for(int n: nums) System.out.printf("%d ", n);
