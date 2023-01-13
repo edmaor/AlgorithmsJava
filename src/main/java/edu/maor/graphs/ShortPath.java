3cfaf17f44de234a7eb7693aac984fa58520cc8f
edu.maor <edmaor128@gmail.com>
2023-01-13 16:07:14 +0100

    Dijkstra (Shortest Path)

--- src/main/java/edu/maor/graphs/ShortPath.java
+++ src/main/java/edu/maor/graphs/ShortPath.java
@@ -17,14 +17,15 @@ public class ShortPath {
             heapifyEdges(edges, len, smallest);
         }
     }
-    public void dijkstra(Graph graph, int node){
+
+    public void dijkstra(Graph graph, int node) {
         boolean[] visited = new boolean[graph.nodes];
         int[] distances = new int[graph.nodes];
 
         visited[node] = true;
         ArrayList<Graph.Edge> queue = new ArrayList<>(graph.adjacencyList[node]);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             heapifyEdges(queue, queue.size(), node);
             // node = queue.remove(0).destination;
             Graph.Edge edge = queue.remove(0);
@@ -41,17 +42,17 @@ public class ShortPath {
 //                }
 //            }
         }
-        for (int i = 0; i < graph.nodes; i++){
+        for (int i = 0; i < graph.nodes; i++) {
             if (visited[i]) System.out.printf(" %d | %d\n", i, distances[i]);
             else System.out.printf(" %d | UNREACHABLE\n", i);
         }
     }
 
-    public void dijkstra(Graph graph, int origin, int destination) {
+    public LinkedList<Graph.Edge> dijkstra(Graph graph, int origin, int destination) {
         boolean[] visited = new boolean[graph.nodes];
         int[] distances = new int[graph.nodes];
         int[] parents = new int[graph.nodes];
-        LinkedList<Graph.Edge> shortestPath =  new LinkedList<>();
+        LinkedList<Graph.Edge> shortestPath = new LinkedList<>();
 
         visited[origin] = true;
         ArrayList<Graph.Edge> queue = new ArrayList<>(graph.adjacencyList[origin]);
@@ -66,42 +67,42 @@ public class ShortPath {
 
                 if (edge.origin != destination) queue.addAll(graph.adjacencyList[edge.destination]);
             }
-
-        }
-        int currentNode = destination;
-        while (currentNode != origin) {
-            shortestPath.addFirst(new Graph.Edge(parents[currentNode], currentNode, distances[currentNode] - distances[parents[currentNode]]));
-            currentNode = parents[currentNode];
         }
         System.out.printf("%d", origin);
         if (visited[destination]) {
+            int currentNode = destination;
+            while (currentNode != origin) {
+                shortestPath.addFirst(new Graph.Edge(parents[currentNode], currentNode, distances[currentNode] - distances[parents[currentNode]]));
+                currentNode = parents[currentNode];
+            }
             for (Graph.Edge edge : shortestPath) {
                 System.out.printf(" --%d--> %d", edge.cost, edge.destination);
             }
             System.out.printf(" | COST: %d", distances[destination]);
+            return shortestPath;
         } else {
-            System.out.printf(" UNREACHABLE %d", destination);
+            System.out.printf(" -- UNREACHABLE --> %d", destination);
+            return null;
         }
-
     }
 
     public static void main(String[] args) {
 
         Graph graph = new Graph(8);
-        graph.addEdge(0,1,15);
-        graph.addEdge(0,5,40);
-        graph.addEdge(0,7,10);
-        graph.addEdge(1,3,25);
-        graph.addEdge(3,2,10);
-        graph.addEdge(3,5,25);
-        graph.addEdge(3,6,30);
-        graph.addEdge(5,1,35);
-        graph.addEdge(6,5,10);
-        graph.addEdge(7,0,10);
+        graph.addEdge(0, 1, 15);
+        graph.addEdge(0, 5, 40);
+        graph.addEdge(0, 7, 10);
+        graph.addEdge(1, 3, 25);
+        graph.addEdge(3, 2, 10);
+        graph.addEdge(3, 5, 25);
+        graph.addEdge(3, 6, 30);
+        graph.addEdge(5, 1, 35);
+        graph.addEdge(6, 5, 10);
+        graph.addEdge(7, 0, 10);
 
         ShortPath shortPath = new ShortPath();
         shortPath.dijkstra(graph, 0);
         System.out.println();
-        shortPath.dijkstra(graph, 0, 5);
+        shortPath.dijkstra(graph, 0, 4);
     }
 }
